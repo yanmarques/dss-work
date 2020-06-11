793474363613af0003903f06397832d959924a08
arturcorreaf <56005912+arturcorreaf@users.noreply.github.com>
2020-06-11 20:34:55 -0300

    Update consistency_checks.sql
    Adicionando venda:cliente

--- consistency_checks.sql
+++ consistency_checks.sql
@@ -39,3 +39,25 @@ SELECT sum(p.vl_par) AS vl_par_previstas FROM parcela p
 WHERE extract(month FROM p.dt_vcto) = 9
 AND extract(year FROM p.dt_vcto) = 2006
 and p.vl_pago IS NULL;
+
+-- venda:cliente
+SELECT sum(p.vl_par) AS nr_par_previstas FROM cliente c
+inner JOIN venda ON c.cd_cli = venda.cd_cli
+INNER JOIN parcela p ON (venda.cd_ven = p.cd_ven AND venda.cd_loj = p.cd_loj)
+WHERE c.cd_cli = 2 AND p.vl_pago IS NULL;
+
+SELECT sum(p.vl_par) AS nr_par_previstas FROM cliente c
+inner JOIN venda ON c.cd_cli = venda.cd_cli
+INNER JOIN parcela p ON (venda.cd_ven = p.cd_ven AND venda.cd_loj = p.cd_loj)
+WHERE c.cd_cli = 6 AND p.vl_pago IS NULL;
+
+SELECT sum(p.vl_par) AS nr_par_previstas FROM cliente c
+inner JOIN venda ON c.cd_cli = venda.cd_cli
+INNER JOIN parcela p ON (venda.cd_ven = p.cd_ven AND venda.cd_loj = p.cd_loj)
+WHERE c.cd_cli = 8 AND p.vl_pago IS NULL;
+
+SELECT sum(p.vl_par) AS nr_par_previstas FROM cliente c
+inner JOIN venda ON c.cd_cli = venda.cd_cli
+INNER JOIN parcela p ON (venda.cd_ven = p.cd_ven AND venda.cd_loj = p.cd_loj)
+WHERE c.cd_cli = 9 AND p.vl_pago IS NULL;
+
